e95dd2c1c26f403083a7475c5c0191d097b62b44
luizmirrelll <aloysius.mirel96@gmail.com>
2020-10-30 02:10:49 +0700

    HTTP Request / Koneksi ke API (Kumpulan Data) 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  PostResult postResult = null;
+  // PostResult postResult = null;
   User user = null;
+  String output = "no data";
 
   @override
   Widget build(BuildContext context) {
@@ -26,21 +27,36 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Text((user != null)
-                  ? user.id + "|" + user.name
-                  : "tidak ada data"),
+              // Text((user != null)
+              //     ? user.id + "|" + user.name
+              //     : "tidak ada data"),
               // +
               // "|" +
               // postResult.job +
               // "|" +
               // postResult.createdAt
-
+              Text(output),
               RaisedButton(
                 onPressed: () {
-                  User.connectToAPI("2").then((value) {
-                    user = value;
+                  User.getUsers("2").then((users) {
+                    output = "";
+                    for (int i = 0; i < users.length; i++)
+                      output = output + "[" + users[i].name + " ]";
                     setState(() {});
                   });
+                  // User.connectToAPI("2").then((value) {
+                  // User.getUsers("2").then((user) {
+                  //   output = "";
+                  //   for (int i = 0; i < user.length; i++)
+                  //     output = output + "[" + user[i].name + " ]";
+                  //   setState(() {});
+                  // });
+                  // user = value;
+                  // setState(() {
+
+                  // }
+                  // );
+                  // );
                   // PostResult.connectToAPI("Badu", "dokter").then((velue) {
                   //   postResult = velue;
                   //   setState(() {});
--- lib/user_model.dart
+++ lib/user_model.dart
@@ -12,13 +12,26 @@ class User {
         id: object['id'].toString(),
         name: object['first_name'] + " " + object['last_name']);
   }
-  static Future<User> connectToAPI(String id) async {
-    String apiURL = "https://reqres.in/api/users/" + id;
+  static Future<List<User>> getUsers(String page) async {
+    String apiURL = "https://reqres.in/api/users?page=" + page;
 
     var apiResult = await http.get(apiURL);
     var jsonObject = json.decode(apiResult.body);
-    var userData = (jsonObject as Map<String, dynamic>)['data'];
+    List<dynamic> listUser = (jsonObject as Map<String, dynamic>)['data'];
 
-    return User.createUser(userData);
+    List<User> users = [];
+    for (int i = 0; i < listUser.length; i++)
+      users.add(User.createUser(listUser[i]));
+
+    return users;
   }
+  // static Future<User> connectToAPI(String id) async {
+  //   String apiURL = "https://reqres.in/api/users/" + id;
+
+  //   var apiResult = await http.get(apiURL);
+  //   var jsonObject = json.decode(apiResult.body);
+  //   var userData = (jsonObject as Map<String, dynamic>)['data'];
+
+  //   return User.createUser(userData);
+  // }
 }
